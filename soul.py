ba721efb457438b04e4c2a6455dc9cb014a96dcd
teamcity10101 <teamcity10101@gmail.com>
2025-01-06 13:02:05 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '8146887226:AAH6y2tDhsRKAQLdUvkrqzbIo91xr4wq7UE'
+TOKEN = '8146887226:AAE9IpR9j16fXuF_7bcg53oOzkh5G3ivZIA'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
 FORWARD_CHANNEL_ID = -1002275420758
 CHANNEL_ID = -1002275420758
